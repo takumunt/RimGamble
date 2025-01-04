5fde13c643b063afded13882d5c449597ed2adb8
takumunt <2131andy@gmail.com>
2025-01-04 22:12:11 +0900

    starting place for the mod; adds a simple Loot Crate that spawns in a random amount of silver on use

---
+++ Source/RimGamble/Class1.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+using RimWorld;
+using Verse;
+
+namespace RimGamble
+{
+    public class CompUseEffect_DispenseRandomItem : CompUseEffect
+    {
+        public override void DoEffect(Pawn usedBy)
+        {
+            base.DoEffect(usedBy); // Does all the effects already in DoEffect, such as camera shake
+
+            // lootcrate behavior
+            if (usedBy.Map == Find.CurrentMap)
+            {
+                // decide what to spawn 
+                // TODO: add random items; for now, just spawn a random amount of silver
+                Thing silverStack = ThingMaker.MakeThing(ThingDefOf.Silver);
+                silverStack.stackCount = UnityEngine.Random.Range(1, 400);
+
+
+                // Spawn the item at the location of the user (pawn)
+                GenSpawn.Spawn(silverStack, usedBy.Position, usedBy.Map);
+            }
+        }
+    }
+}
