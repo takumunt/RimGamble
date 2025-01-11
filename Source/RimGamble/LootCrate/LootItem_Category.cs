6096dbe77456ca5b82e3c0033e555439d823ea6d
takumunt <2131andy@gmail.com>
2025-01-11 23:40:34 +0900

    added ability for loot crate xml files to specify what the maximum tech level for a given lootitem_category can be

--- Source/RimGamble/LootCrate/LootItem_Category.cs
+++ Source/RimGamble/LootCrate/LootItem_Category.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using RimWorld;
 using Verse;
 
 namespace RimGamble
@@ -13,5 +14,6 @@ namespace RimGamble
         public List<ThingDef> exclude = new List<ThingDef>();
         public int rareModif = 2; // variable used to change how the random generation of quality is handled (where applicable) the given value is where quality will cluster around
         public float widthFactor = 0; // variable used to change how clustered the random generation of quality is handled
+        public TechLevel maxTechLevelGenerate = TechLevel.Archotech; // max level of technology the item collection can be
     }
 }
